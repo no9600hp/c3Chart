3991c5230ee235e019d68b637e4c9935c7dca355
Minhaz Uddin <minhaz@httechus.com>
2020-11-08 19:26:25 -0600

    added c3Chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as c3 from 'c3'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'c3ChartTutorial';
+export class AppComponent implements OnInit {
+  constructor() {}
+  title = 'c3ChartTutorial'
+  ngOnInit() {
+    this.createChart()
+  }
+  createChart() {
+    var chart = c3.generate({
+      bindto: '#timeseriesChart',
+      data: {
+        x: 'x',
+        columns: [
+          [
+            'x',
+            '2013-01-01',
+            '2013-01-02',
+            '2013-01-03',
+            '2013-01-04',
+            '2013-01-05',
+            '2013-01-06'
+          ],
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    })
+  }
 }
